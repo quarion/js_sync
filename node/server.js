a287859fb58642a04c351f63eaad4b98894e9672
Filip Gibki <filip.gibki@wincor-nixdorf.com>
2014-05-13 13:19:17 +0200

    DB connectivity with mongoskin; basic test REST API

--- node/server.js
+++ node/server.js
@@ -1,21 +1,52 @@
 console.log('Server starting...');
-
-var mongoose = require('mongoose');  
+ 
 var express = require('express');
+var mongo = require('mongoskin');
+var bodyParser = require('body-parser');
 
-var connectionString = 'mongodb://127.0.0.1/syncDB'; //process.env.CUSTOMCONNSTR_MONGOLAB_URI
-mongoose.connect(connectionString);
+var connectionString = process.env.CUSTOMCONNSTR_MONGOLAB_URI || 'mongodb://127.0.0.1/syncDB';
 
 var app = express()
   , http = require('http')
   , server = http.createServer(app); 
 
-var port = 8080;//process.env.PORT || 8080;
+app.use(bodyParser());
+  
+var port = process.env.PORT || 8080;
 server.listen(port);
 
 app.get('/', function (req, res) {
-	console.log('Request recieved');
 	res.sendfile(__dirname + '/index.html');
 });
 
-console.log('Server is running.');
+
+app.post('/insert', function(req, res) {
+    var data = req.body;
+    
+	console.log( 'Adding: ' + JSON.stringify(data) );
+	
+	var db = mongo.db(connectionString, {native_parser:true});
+	
+	db.collection('data').insert(data, function(err, result) {
+		if (err) throw err;
+		if (result) console.log('Added!');
+		
+		res.send('Ok');
+	});
+
+});
+
+app.post('/fetchAll', function(req, res) {
+
+	var db = mongo.db(connectionString, {native_parser:true});
+	
+	db.collection('data').find().toArray(function(err, result) {
+		if (err) throw err;
+		console.log(result);
+		res.json(result);
+	});
+
+});
+
+
+console.log('Server is running on port ' + port);
