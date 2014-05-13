1c7b432fd131b4fb8d9f4960c2ea8e0adeff55b1
Filip Gibki <filip.gibki@wincor-nixdorf.com>
2014-05-13 16:03:53 +0200

    added readme; added script to start database service and node server

---
+++ readme.txt
@@ -0,0 +1,15 @@
+Configuration instruction for Windows
+
+1. Install Windows version of node.js
+2. Install Windows version of MongoDB
+3. Create database http://docs.mongodb.org/manual/tutorial/install-mongodb-on-windows/
+4. Install node.js packages:
+	- express
+	- express body parsers
+	- mongoskin
+	
+	
+To run test server
+1. Start mongod process
+3. start node with main server script
+Project contains PowerShell ISE script to start both services in single window
---
+++ runNodeServer.ps1
@@ -0,0 +1,22 @@
+﻿#TODO: extract directories as parameters
+
+
+#Start MongoDB
+$mongoTab = $psISE.PowerShellTabs.Add()
+$mongoTab.DisplayName = "Mongod";
+
+# Wait for the new tab to be ready to run more commands.
+While (-not $mongoTab.CanInvoke) {
+    Start-Sleep -m 100
+}
+
+$mongoTab.Invoke({ cd "C:\Program Files\MongoDB 2.6 Standard\bin\";  ./mongod.exe --dbpath "C:\projects\mongo\data"})
+
+
+#Start Node
+$nodeTab = $psISE.PowerShellTabs.Add()
+$nodeTab.DisplayName = "Node.js"
+While (-not $nodeTab.CanInvoke) {
+    Start-Sleep -m 100
+}
+$nodeTab.Invoke({cd C:\projects\sync\node ; node server.js})
